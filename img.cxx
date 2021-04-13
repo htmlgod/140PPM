67c9ec17092dd6f3ded21fe91fdf477e3c596adb
htmlgod <ivannevtrilov212@gmail.com>
2021-04-14 00:21:56 +0300

    (img) thick line draw

--- img.cxx
+++ img.cxx
@@ -32,20 +32,31 @@ int main() {
 		framebuffer[i].g = 0;
 		framebuffer[i].b = 90;
 	}
-	render("singlecolor.ppm", framebuffer);
+	//render("singlecolor.ppm", framebuffer);
 
-	// draw green diagonal line
-	for (size_t y = 0; y < HEIGHT; y++) {
-		for (size_t x = 0; x < WIDTH; x++) {
+	// draw green thick diagonal line
+	framebuffer[0].r = 0;
+	framebuffer[0].b = 0;
+	framebuffer[0].g = 255;
+	for (size_t y = 1; y < HEIGHT; y++) {
+		for (size_t x = 1; x < WIDTH; x++) {
 			if (x == y) { 
 				framebuffer[x + y*WIDTH].r = 0;
 				framebuffer[x + y*WIDTH].b = 0;
 				framebuffer[x + y*WIDTH].g = 255;
+				
+				framebuffer[x-1 + y*WIDTH].r = 0;
+				framebuffer[x-1 + y*WIDTH].b = 0;
+				framebuffer[x-1 + y*WIDTH].g = 255;
+
+				framebuffer[x + (y-1)*WIDTH].r = 0;
+				framebuffer[x + (y-1)*WIDTH].b = 0;
+				framebuffer[x + (y-1)*WIDTH].g = 255;
 			}
 		}
 	}
 
-	render("greenline.ppm", framebuffer);
+	render("thickgreenline.ppm", framebuffer);
 	
 	delete[] framebuffer;
 
